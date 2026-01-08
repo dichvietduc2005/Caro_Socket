6bb778affd201ef621938e366c0b5c8b8686a368
TN-Kieran <trognghia284@gmail.com>
2026-01-09 00:17:50 +0700

    thiết lặp socket

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -1,4 +1,12 @@
 package server;
 
+import server.network.ServerSocketManager;
+
 public class MainServer {
+    public static void main(String[] args) {
+        // Khởi động server quản lý socket trên port 12345 (hoặc port bạn chọn)
+        int port = 12345;
+        ServerSocketManager serverManager = new ServerSocketManager(port);
+        serverManager.startServer();
+    }
 }
